fe16eecf81c38328dbb7b8e9273a95cd92e8e09a
Ashwani Yadav <ashwani.yadav@ezeiatech.in>
2024-01-24 12:00:44 +0530

    mvc

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: function () {
+                return this.password === this.confirmPassword
+            },
+            message: "Password and Confirm Password should be same"
+        }
+    },
+    address: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
