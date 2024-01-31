4498071197be864f6178f900b16c74b8688f0525
Ashwani Yadav <ashwani.yadav@ezeiatech.in>
2024-01-31 13:13:03 +0530

    added product schema

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,83 +1,17 @@
 const Product = require('../models/productModel')
+const {
+    checkInput,
+    getAllFactory,
+    getElementByIdFactory,
+    createElementFactory
+} = require('../utils/crudFactory');
 
-const checkInput = function (req, res, next) {
-    const productDetails = req.body;
-    const isEmpty = Object.keys(productDetails).length === 0;
-    if (isEmpty) {
-        res.status(400).json({
-            status: 400,
-            message: "Body can not be empty"
-        })
-    }
-    else {
-        next();
-    }
-}
 /**handlers */
 /** Route handlers */
 
-async function getProductHandler(req, res) {
-    try {
-        const productData = await Product.find();
-        if (productData.length === 0)
-            throw new Error("No Product found");
-        else {
-            msg = 'Data Found'
-            res.json({
-                status: 'success',
-                statusCode: 200,
-                message: msg,
-                data: productData
-
-            })
-        }
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message
-        })
-    }
-}
-
-async function createProductHandler(req, res) {
-    try {
-        const productDetails = req.body;
-        const product = await Product.create(productDetails);
-        res.status(200).json({
-            message: "Product created successfully",
-            data: product
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-    // const id = short.generate();
-
-}
-
-async function getProductById(req, res) {
-    try {
-        const { id } = req.params;
-        // const Product = ProductData.find((Product) => Product.id === id);
-        const product = await Product.findById(id);
-        if (!product) {
-            throw new Error("Product not found");
-        }
-        res.json({
-            status: 200,
-            message: "Product Found",
-            data: Product
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-}
+const getProductHandler = getAllFactory(Product);
+const getProductById = getElementByIdFactory(Product);
+const createProductHandler = createElementFactory(Product);
 
 async function updateProductById(req, res) {
     try {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,83 +1,18 @@
 const User = require('../models/userModel')
+const {
+    checkInput,
+    getAllFactory,
+    getElementByIdFactory,
+    createElementFactory,
+    updateElementByIdFactory
+} = require('../utils/crudFactory')
 
-const checkInput = function (req, res, next) {
-    const userDetails = req.body;
-    const isEmpty = Object.keys(userDetails).length === 0;
-    if (isEmpty) {
-        res.status(400).json({
-            status: 400,
-            message: "Body can not be empty"
-        })
-    }
-    else {
-        next();
-    }
-}
 /**handlers */
 /** Route handlers */
 
-async function getUserHandler(req, res) {
-    try {
-        const userData = await User.find();
-        if (userData.length === 0)
-            throw new Error("No user found");
-        else {
-            msg = 'Data Found'
-            res.json({
-                status: 'success',
-                statusCode: 200,
-                message: msg,
-                data: userData
-
-            })
-        }
-    } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: error.message
-        })
-    }
-}
-
-async function createUserHandler(req, res) {
-    try {
-        const userDetails = req.body;
-        const user = await User.create(userDetails);
-        res.status(200).json({
-            message: "User created successfully",
-            data: user
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-    // const id = short.generate();
-
-}
-
-async function getuserById(req, res) {
-    try {
-        const { id } = req.params;
-        // const user = userData.find((user) => user.id === id);
-        const user = await User.findById(id);
-        if (!user) {
-            throw new Error("User not found");
-        }
-        res.json({
-            status: 200,
-            message: "User Found",
-            data: user
-        });
-    } catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message
-        })
-    }
-}
+const getUserHandler = getAllFactory(User);
+const getuserById = getElementByIdFactory(User);
+const createUserHandler = createElementFactory(User);
 
 async function updateUserById(req, res) {
     try {
